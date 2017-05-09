e2df7fec0d756396be04977d33ad919959ef45a4
Daniel Parreira <daniel@Daniels-MacBook-Pro.local>
2017-05-09 00:14:57 -0300

    Conectar lista de cursos com component

--- src/components/lista_cursos.js
+++ src/components/lista_cursos.js
@@ -23,10 +23,39 @@ class ListaCursos extends Component {
 
 	renderizarListaDeCursos() {
 		const cursos = this.state.cursos.map((curso) => {
-			return (<li key={curso.id_curso}>{curso.nome_curso}</li>);
+			return (
+				<span>
+					<a href={"#" + curso.id_curso} 
+					   className="list-group-item list-group-item-warning" 
+					   data-toggle="collapse" 
+					   data-parent="#ListaDeCursos"
+					   key={curso.id_curso}
+					>
+					<b>{curso.nome_curso}</b>
+					</a>
+					<div className="collapse" id={curso.id_curso}>
+			        	<a href={"#SubMenu" + curso.id_curso} 
+			        	   className="list-group-item" 
+			        	   data-toggle="collapse" 
+			        	   data-parent={"#SubMenu" + curso.id_curso}>
+			        	   Subitem {curso.id_curso} <i className="fa fa-caret-down"></i>
+			        	</a>
+			        	<div className="collapse list-group-submenu" 
+			        		 id={"SubMenu" + curso.id_curso}>
+			            	<a href="#" className="list-group-item" data-parent={"#SubMenu" + curso.id_curso}>Descricao do curso</a>
+			        	</div>
+			        </div>
+		        </span>
+			);
 		});
 
-		return (<ul>{cursos}</ul>);
+		return (
+		<div id="ListaDeCursos">
+			<div class="list-group panel">
+				{cursos}
+			</div>
+		</div>
+		);
 	}
 
 	render() {
